e757d35655ff03a1dfd58ae05345e02d6fa3a6c2
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-01 01:47:29 +0100

    Add project files.

---
+++ LibToTasks/Validation/Transformator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NLog;
+
+namespace LibToTasks.Validation
+{
+    public class Transformator
+    {
+        private ILogger _logger;
+
+        public Transformator() 
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public TResult ConfirmConversion<TResult, TValue>(TValue valueToConverting)
+        {
+            if (valueToConverting == null)
+            {
+                _logger.Error("Value {0} cannot be converted to {1}", valueToConverting, typeof(TResult));
+                throw new FormatException($"We cannot convert this value to {typeof(TResult)}");
+            }
+
+            try
+            {
+                TResult result = (TResult)Convert.ChangeType(valueToConverting, typeof(TResult));
+                _logger.Info("Value {0} can be converted to {1}", valueToConverting, typeof(TResult));
+
+                return result;
+            }
+            catch
+            {
+                _logger.Error("Value {0} cannot be converted to {1}", valueToConverting, typeof(TResult));
+            }
+
+            throw new FormatException($"We cannot convert this value to {typeof(TResult)}");
+        }
+    }
+}
---
+++ LibToTasks/Validation/Validator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NLog;
+
+namespace LibToTasks.Validation
+{
+    public class Validator 
+    {
+        private  ILogger _logger;
+
+        public Validator() 
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public T CheckValue<T>(Func<T, bool> checking, T valueToChecking, bool withException = true) 
+        {
+            if (checking.Invoke(valueToChecking)) 
+            {
+                _logger.Info("Value {0} passed checking", valueToChecking);
+
+                return valueToChecking;
+            }            
+
+            _logger.Error("Value {0} didn`t pass checking", valueToChecking);
+
+            if (withException)
+            {
+                throw new FormatException("Yuor value too small or big");
+            }
+
+            return default;
+        }
+    }
+}
